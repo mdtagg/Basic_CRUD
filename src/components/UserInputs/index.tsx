ab0a8c5e8971bfb4056d50f043fd389c5314bc19
Mike Tagg <mdtagg91@gmail.com>
2023-06-28 16:33:34 -0700

    submit button and table boilerplate html added

--- src/components/UserInputs/index.tsx
+++ src/components/UserInputs/index.tsx
@@ -4,11 +4,12 @@ const UserInputs = () => {
     return (
         <form>
             <label htmlFor="fName">First Name</label>
-            <input id="fName" type = "text" placeholder='Enter your first name'></input>
+            <input id="fName" type="text" placeholder='Enter your first name'></input>
             <label htmlFor="lName">Last Name</label>
             <input id="lName" type="text" placeholder='Enter your last name'></input>
             <label htmlFor="email">Email</label>
             <input id="email" type='email' placeholder='Enter your email'></input>
+            <button>Submit</button>
         </form>
     )
 }
--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,9 +1,25 @@
 
-
+import './index.css'
 const UserList = () => {
     return (
-        <div></div>
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Actions</th>
+            </tr>
+        </table>
     )
 }
 
 export default UserList
+
+{/* <section>
+            <div className="list-titles">
+                <p>First Name</p>
+                <p>Last Name</p>
+                <p>Email</p>
+                <p>Actions</p>
+            </div>
+        </section> */}
